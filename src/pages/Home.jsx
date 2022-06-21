76a58e80d31e4de2e4befdf32f388fabe0a7bafc
8pro <mechackkobius@gmail.com>
2022-06-21 21:35:41 +0300

    updates to fix some issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../components/About";
 import Header from "../components/Header";
+import Resume from "../components/Resume";
 
 const Home = () => {
   return (
     <>
       <Header />
       <About />
+      <Resume />
     </>
   );
 };
