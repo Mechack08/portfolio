208f98420f32d4dffbfbbb81aadb348577705f35
8pro <mechackkobius@gmail.com>
2022-06-21 21:28:27 +0300

    Created fake data to handle

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export const userExperience = [
+  {
+    date: "2021 - Current",
+    institut: "Freelance",
+    position: "Web developer - UI/UX Designer",
+    description:
+      "As a web developer I worked on several projects, sometimes under clients or team mates pressure, despite all those pressures I'm happy because it made me stronger. ❤",
+  },
+  {
+    date: "2019 - 2020",
+    institut: "Training",
+    position: "Web developer",
+    description:
+      "A professional training, after getting my bachelor. I learned how to work as a professional web developer.",
+  },
+];
+
+export const userEducation = [
+  {
+    date: "2018 - 2020",
+    institut: "University - Master",
+    position: "Computer Engineering",
+    description:
+      "Adventist University of Lukanga.  Butembo, Democratic Republuc of Congo",
+  },
+  {
+    date: "2015 - 2018",
+    institut: "University - Bachelor",
+    position: "Economics and computer management",
+    description:
+      "Adventist University of Lukanga.  Butembo, Democratic Republuc of Congo",
+  },
+];
+
+export const codeSkills = [
+  {
+    subject: "HTML",
+    level: "95",
+  },
+  {
+    subject: "CSS",
+    level: "90",
+  },
+  {
+    subject: "JavaScript",
+    level: "87",
+  },
+  {
+    subject: "ReactJS",
+    level: "90",
+  },
+  {
+    subject: "Saas",
+    level: "92",
+  },
+  {
+    subject: "Redux",
+    level: "85",
+  },
+  {
+    subject: "NodeJS",
+    level: "80",
+  },
+  {
+    subject: "PHP",
+    level: "74",
+  },
+  {
+    subject: "MySQL",
+    level: "93",
+  },
+  {
+    subject: "MongoDB",
+    level: "64",
+  },
+];
+
+export const designSkills = [
+  {
+    subject: "AdobeXD",
+    level: "87",
+  },
+  {
+    subject: "Figma",
+    level: "52",
+  },
+  {
+    subject: "PhotoShop",
+    level: "47",
+  },
+];
