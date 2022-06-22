6bd26af03ad0e44711122dbc74e1e6bf59bd64c5
8pro <mechackkobius@gmail.com>
2022-06-22 16:11:23 +0300

    Contact component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "../components/About";
+import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Portfolio from "../components/Portfolio";
 import Resume from "../components/Resume";
@@ -11,6 +12,7 @@ const Home = () => {
       <About />
       <Resume />
       <Portfolio />
+      <Contact />
     </>
   );
 };
