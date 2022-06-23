be6e6ae160cb814696cf8d01c14f9b9641bbcb25
8pro <mechackkobius@gmail.com>
2022-06-23 12:33:00 +0300

    Add Footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../components/About";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Portfolio from "../components/Portfolio";
 import Resume from "../components/Resume";
@@ -13,6 +14,7 @@ const Home = () => {
       <Resume />
       <Portfolio />
       <Contact />
+      <Footer />
     </>
   );
 };
