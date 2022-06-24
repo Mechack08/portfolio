a37c221e4ce1e33e25cf7f9c9f8cc9daa3ab5ecb
8pro <mechackkobius@gmail.com>
2022-06-24 16:00:47 +0300

    Responsive by adding an humberger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiSun } from "react-icons/fi";
 import { HiMoon } from "react-icons/hi";
 import Logo from "../../src/assets/Logo.svg";
 
 const Navbar = () => {
+  const [widthSize, setWidthSize] = useState(window.innerWidth);
+  const [menuClass, setMenuClass] = useState("menu-humberger none");
+  const [hideMenus, setHideMenus] = useState("hide");
+
+  const handleClass = () => {
+    if (menuClass === "menu-humberger none") {
+      setMenuClass("menu-humberger");
+    } else if (menuClass === "menu-humberger") {
+      setMenuClass("menu-humberger cross");
+      setHideMenus("");
+    } else {
+      setMenuClass("menu-humberger");
+      setHideMenus("hide");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      const size = window.innerWidth;
+      setWidthSize(size);
+
+      if (size > 810) {
+        setMenuClass("menu-humberger none");
+        setHideMenus("hide");
+      } else {
+        setMenuClass("menu-humberger");
+        setHideMenus("hide");
+      }
+    });
+  }, [widthSize]);
+
   return (
     <div className="navbar-container">
       <div className="logo">
@@ -17,7 +48,7 @@ const Navbar = () => {
           <FiSun className="show-icon" />
           <HiMoon className="hide-icon" />
         </div>
-        <nav>
+        <nav className={hideMenus}>
           <span>
             <a a href="#home" className="active">
               Home
@@ -36,6 +67,9 @@ const Navbar = () => {
             <Link to="/blog">Blog</Link>
           </span>
         </nav>
+        <div className={menuClass} onClick={handleClass}>
+          <div className="middle-line"></div>
+        </div>
       </div>
     </div>
   );
