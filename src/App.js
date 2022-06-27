6f359cdce91f37dad1b7c4b22a3a2e4a421305c3
8pro <mechackkobius@gmail.com>
2022-06-27 15:19:41 +0300

    Add new route /blog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/blog" element={<Blog />} />
     </Routes>
   );
 }
