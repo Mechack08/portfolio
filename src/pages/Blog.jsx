c42d0b497c560fd86c86e60c67c1a5526f2b7852
8pro <mechackkobius@gmail.com>
2022-06-27 15:21:16 +0300

    Working on Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import ArticlesList from "../components/ArticlesList";
+import Navbar from "../components/Navbar";
+import SearchBar from "./SearchBar";
+import { articles } from "../data";
+import Footer from "../components/Footer";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <div className="blog-container">
+        <SearchBar />
+        <ArticlesList data={articles} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Blog;
