d1146dfceb24bc3957bbb2c4f541f1399e59485b
8pro <mechackkobius@gmail.com>
2022-06-29 14:35:10 +0300

    SingleArticle.jsx HTML structure created

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import CommentForm from "./CommentForm";
+
+const SingleArticle = ({ data }) => {
+  console.log(data);
+  return (
+    <article className="container">
+      <span className="title">{data.title}</span>
+      <span className="details">
+        Published on {data.date} - By {data.author}
+      </span>
+      <span className="lang">{data.lang}</span>
+      <span className="category">{data.category}</span>
+      <div className="img-container">
+        <img src={`/${data.img}`} alt={data.title} />
+      </div>
+      <p>{data.content}</p>
+      <CommentForm />
+    </article>
+  );
+};
+
+export default SingleArticle;
