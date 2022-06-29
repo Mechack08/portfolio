19ba6be6b69ad7882bb9dc71fd0b9cca7b087d9b
8pro <mechackkobius@gmail.com>
2022-06-29 14:19:37 +0300

    Add Article component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
+import Article from "./pages/Article";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/blog" element={<Blog />} />
+      <Route exact path="/blog/:id" element={<Article />} />
     </Routes>
   );
 }
