b24ea0e7f51642d22085dfa5c358e75dcbbdf405
8pro <mechackkobius@gmail.com>
2022-06-29 14:23:30 +0300

    Article component created

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import SingleArticle from "../components/SingleArticle";
+import { articles } from "../data";
 
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <SingleArticle data={articles[0]} />
+      <Footer />
+    </div>
+  );
 };
 
 export default Article;
