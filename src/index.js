83b0bdbc3d7a8330762a96474db373db5b509b3d
8pro <mechackkobius@gmail.com>
2022-08-14 09:19:34 +0300

    Add Redux in the index.js file to get user infos when app is loaded

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 
+import { getUser } from "./actions/user.action";
+
+/* Redux in project */
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+
+import rootReducers from "./reducers";
+
+/* dev tools */
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getUser());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
