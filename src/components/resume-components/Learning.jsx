eb4d6e57923a0a01352038524caa7178f080c6bf
8pro <mechackkobius@gmail.com>
2022-08-14 16:52:00 +0300

    updates, change fake data to db data. handle some issues

--- src/components/resume-components/Learning.jsx
+++ src/components/resume-components/Learning.jsx
@@ -4,18 +4,23 @@ const Learning = ({ title, data }) => {
   return (
     <div className="learning-container">
       <span className="title-underline">{title}</span>
-      {data.map(({ date, institut, position, description }, index) => {
-        return (
-          <div className="learning-content" key={index}>
-            <div className="date-position">
-              <span>{date}</span>
-              <span>{institut}</span>
+
+      {data ? (
+        data.map(({ _id, period, institution, position, description }) => {
+          return (
+            <div className="learning-content" key={_id}>
+              <div className="date-position">
+                <span>{period}</span>
+                <span>{institution}</span>
+              </div>
+              <span>{position}</span>
+              <p>{description}</p>
             </div>
-            <span>{position}</span>
-            <p>{description}</p>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 };
