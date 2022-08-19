127b101a36effffd30dea85ee024f658443c09ec
8pro <mechackkobius@gmail.com>
2022-08-19 07:49:31 +0300

    update component. Replace index while displaying data by _id from the DB

--- src/components/resume-components/Skills.jsx
+++ src/components/resume-components/Skills.jsx
@@ -4,9 +4,9 @@ const Skills = ({ title, data }) => {
   return (
     <div className="skills-container">
       <span className="title-underline">{title}</span>
-      {data.map(({ subject, level }, index) => {
+      {data.map(({ _id, subject, level }) => {
         return (
-          <div className="skill" key={index}>
+          <div className="skill" key={_id}>
             <div className="skill-header">
               <span className="skill-name">{subject}</span>
               <span className="skill-pourcentage">{`${level}%`}</span>
