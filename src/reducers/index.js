cce9ffa3f5e0502ba83cb9a92a5adc631a01a4f5
8pro <mechackkobius@gmail.com>
2022-08-19 07:54:12 +0300

    add new path to get portfolios

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import backgroundReducer from "./background.reducer";
 import skillReducer from "./skill.reducer";
 import userReducer from "./user.reducer";
+import portfolioReducer from "./portfolio.reducer";
 
 export default combineReducers({
   backgroundReducer,
   skillReducer,
   userReducer,
+  portfolioReducer,
 });
